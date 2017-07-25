3e633947ef4a21e17d94371eb9578a8d01413f57
James Baxley <james@meteor.com>
2017-07-25 10:31:53 -0400

    Revert "moved project to use lerna"

--- packages/apollo-link/src/index.ts
+++
@@ -1,8 +0,0 @@
-import { execute, ApolloLink } from './link';
-export { makePromise } from './linkUtils';
-export * from './types';
-
-import * as Observable from 'zen-observable';
-
-export default ApolloLink;
-export { Observable, ApolloLink, execute };
